44d90dd89f515f5e9e7cd7c65ce20ed1506def47
François Kooman <fkooman@tuxed.net>
2015-10-23 12:52:09 +0200

    add license to SimpleTpl and autofocus the username field

--- example/SimpleTpl.php
+++ example/SimpleTpl.php
@@ -1,5 +1,20 @@
 <?php
 
+/**
+ * Copyright 2015 François Kooman <fkooman@tuxed.net>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 use fkooman\Tpl\TemplateManagerInterface;
 
 class SimpleTpl implements TemplateManagerInterface
@@ -37,7 +52,7 @@ class SimpleTpl implements TemplateManagerInterface
                             <h2>Sign In</h2>
                             <p>Please sign in with your username and password.</p>
                             <form method="post" action="_auth/form/verify">
-                                <input type="text" name="userName" placeholder="Username" value="%s" required>
+                                <input type="text" name="userName" placeholder="Username" value="%s" autofocus required>
                                 <input type="password" name="userPass" placeholder="Password" required>
                                 <button type="submit">Sign in</button>
                             </form>
